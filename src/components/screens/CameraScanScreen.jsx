1a885a367afafcd2865925d62a872c044d179757
AndrewAnania <android031894@gmail.com>
2024-02-26 16:02:33 -0600

    added manual input

--- src/components/screens/CameraScanScreen.jsx
+++ src/components/screens/CameraScanScreen.jsx
@@ -1,9 +1,46 @@
-import { Pressable, Text, View, Button } from "react-native";
+import { Text, View, Button, Modal, Pressable, TextInput } from "react-native";
+import React from 'react';
+import { useState } from "react";
 import Styles from "../../styles/main";
 
 function CameraScanScreen({ navigation }) {
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [text, onChangeText] = React.useState('');
+
     return (
         <View style={[Styles.container]}>
+
+            <Modal
+                animationType="slide"
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(false);
+                }}>
+                <View style={[Styles.container]}>
+                    <View style={[Styles.container]}>
+                        <Text style={[Styles.hero]}>Enter patient ID number:</Text>
+                        <TextInput
+                            style={[Styles.input]}
+                            onChangeText={onChangeText}
+                            value={text}
+                            placeholder="input id here"
+                        ></TextInput>
+                        <Pressable
+                            onPress={() => {
+                                navigation.push("Confirm Patient")
+                                setModalVisible(o => !o)
+                            }}>
+                            <Text style={{ color: 'green' }}>Confirm</Text>
+                        </Pressable>
+                        <Pressable
+                            onPress={() => setModalVisible(o => !o)}>
+                            <Text style={{ color: 'red' }}>Go back</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+
             <Text style={[Styles.h4]}>Patient Select</Text>
             <Text style={[Styles.h6]}>Scan a patient's barcode to continue</Text>
             <Pressable onPress={() => navigation.push("Confirm Patient")}>
@@ -14,7 +51,7 @@ function CameraScanScreen({ navigation }) {
                 </View>
             </Pressable>
             <Text style={[Styles.h5, { marginTop: 50 }]}>Not working?</Text>
-            <Button title="Enter manually" onPress={() => alert("Not yet implemented")} />
+            <Button title="Enter manually" onPress={() => setModalVisible(true)} />
         </View>
     );
 }
--- src/components/screens/PatientSelectScreen.jsx
+++ src/components/screens/PatientSelectScreen.jsx
@@ -1,13 +1,48 @@
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, Modal, Pressable, TextInput } from "react-native";
+import React from 'react';
+import { useState } from "react";
 import Styles from "../../styles/main";
 
 function PatientSelectScreen({ navigation }) {
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [text, onChangeText] = React.useState('');
+
     return (
         <View style={[Styles.container]}>
+            <Modal
+                animationType="slide"
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(false);
+                }}>
+                <View style={[Styles.container]}>
+                    <View style={[Styles.container]}>
+                        <Text style={[Styles.hero]}>Enter patient ID number:</Text>
+                        <TextInput
+                            style={[Styles.input]}
+                            onChangeText={onChangeText}
+                            value={text}
+                            placeholder = "input id here"
+                        ></TextInput>
+                        <Pressable
+                            onPress={() => {
+                                navigation.push("Confirm Patient")
+                                setModalVisible(o => !o)
+                                } }>
+                            <Text style={{color: 'green'}}>Confirm</Text>
+                        </Pressable>
+                        <Pressable
+                            onPress={() => setModalVisible(o => !o)}>
+                                <Text style={{color: 'red'}}>Go back</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
             <Text style={[Styles.h4]}>Patient Select</Text>
             <Text style={[Styles.h6]}>Choose how you want to input patient information</Text>
             <Button title="Scan patient barcode" onPress={() => navigation.push("Scan Barcode")} />
-            <Button title="Manually enter patient information" onPress={() => alert("Not yet implemented")} />
+            <Button title="Manually enter patient information" onPress={() => setModalVisible(true)} />
         </View>
     );
 }
