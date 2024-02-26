251a00dd8fd1ca7fdf2634ece490899ea8a6572f
cxhuy <alvinkang1@gmail.com>
2024-02-26 16:30:51 -0600

    change stylesheet

--- src/styles/main.js
+++ src/styles/main.js
@@ -5,13 +5,16 @@ const consts = {
 };
 
 const colors = {
-    GEPurple: "#5A0CB5"
+    GEPurple: "#5A0CB5",
+    TextColor: "#D7D7D7"
 };
 
 const Styles = StyleSheet.create({
     colors,
     consts,
     container: {
+        backgroundColor: "#1A1B1C",
+        height: "100%",
         display: "flex",
         flexDirection: "column",
         gap: consts.gapIncrement,
@@ -25,21 +28,27 @@ const Styles = StyleSheet.create({
         // fontFamily: "Inter"
     },
     h1: {
+        color: colors.TextColor,
         fontSize: 64
     },
     h2: {
+        color: colors.TextColor,
         fontSize: 56
     },
     h3: {
+        color: colors.TextColor,
         fontSize: 48
     },
     h4: {
+        color: colors.TextColor,
         fontSize: 32
     },
     h5: {
+        color: colors.TextColor,
         fontSize: 24
     },
     h6: {
+        color: colors.TextColor,
         fontSize: 16
     },
     screenSkeleton: {
