880e10dc3ad12d0814e8a4a57bcb71175e2fc161
cxhuy <alvinkang1@gmail.com>
2024-03-04 14:14:53 -0600

    feat: change design of patient confirm screen

--- src/components/screens/PatientConfirmScreen.jsx
+++ src/components/screens/PatientConfirmScreen.jsx
@@ -13,21 +13,23 @@ function PatientConfirmScreen({ navigation }) {
     return (
         // Maybe we could pre-populate the manual information page with the results from the scan on this page? ~mr
         <View style={[Styles.container]}>
-            <Text style={[Styles.h4, Styles.underline]}>Patient Select</Text>
+            <Text style={[Styles.h4]}><Text style={{color: "white", fontWeight: "bold"}}>Patient Select</Text></Text>
             <Text style={[Styles.h6]}>Is this the right patient?</Text>
+            <View style={{height: 10}}></View>
             <View style={[Styles.container, { width: 250, height: 250, backgroundColor: Styles.colors.GEPurple }]}>
                 <Text style={{ color: "white", textAlign: "center", padding: 50 }}>(patient picture)</Text>
             </View>
+            <View style={{height: 10}}></View>
             <Text style={[Styles.h5]}>
                 {patientProfile.lastName}, {patientProfile.firstName}
             </Text>
             <Text style={[Styles.h5]}>{patientProfile.dob}</Text>
             <Text style={[Styles.h6]}>MRN: {patientProfile.mrn}</Text>
             <Text style={[Styles.h6]}>Visit number: {patientProfile.visitNumber}</Text>
-
+            <View style={{height: 10}}></View>
             <Button title="Yes" onPress={() => navigation.push("Confirm Link")} />
             <Button title="No, scan again" onPress={() => navigation.pop()} />
-            
+            <View style={{height: 10}}></View>
         </View>
     );
 }
