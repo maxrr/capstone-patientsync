fae580c36c7fff55c2051313c3ad1487e9af6d8d
AndrewAnania <android031894@gmail.com>
2024-03-05 23:21:07 -0600

    Both scan barcode and input manually go to CameraScanScreen now

--- src/components/screens/PatientSelectScreen.jsx
+++ src/components/screens/PatientSelectScreen.jsx
@@ -10,48 +10,18 @@ function PatientSelectScreen({ navigation }) {
 
     return (
         <View style={[Styles.container]}>
-            <Modal
-                animationType="slide"
-                visible={modalVisible}
-                onRequestClose={() => {
-                    setModalVisible(false);
-                }}>
-                <View style={[Styles.container]}>
-                    <View style={[Styles.container]}>
-                        <Text style={[Styles.hero]}>Enter patient ID number:</Text>
-                        <TextInput
-                            style={[Styles.input]}
-                            onChangeText={onChangeText}
-                            value={text}
-                            placeholder = "input id here"
-                        ></TextInput>
-                        <Pressable
-                            onPress={() => {
-                                navigation.push("Confirm Patient")
-                                setModalVisible(o => !o)
-                                } }>
-                            <Text style={{color: 'green'}}>Confirm</Text>
-                        </Pressable>
-                        <Pressable
-                            onPress={() => setModalVisible(o => !o)}>
-                                <Text style={{color: 'red'}}>Go back</Text>
-                        </Pressable>
-                    </View>
-                </View>
-            </Modal>
             <Text style={[Styles.h4]}><Text style={{color: "white", fontWeight: "bold"}}>Patient Select</Text></Text>
             <Text style={[Styles.h6]}>Choose method to input patient info</Text>
 
             {/*Updating buttons for Patient Select screen -DT */}
             {/**Using Pressable instead of button because then we can customize it w/ stylesheet -DT */}
-            {/*NOTE: can we pop instead of push here? -AA*/}
-            <Pressable style={Styles.button} onPress={() => navigation.push("Scan Barcode")}>
+            <Pressable style={Styles.button} onPress={() => navigation.push("Scan Barcode", {manual: false})}>
                 <Text style={[Styles.button, Styles.buttonText, { backgroundColor: Styles.colors.GEPurple}]}>Scan Patient Barcode</Text>
             </Pressable>
 
             <Text style={[Styles.h5]}>or</Text>
 
-            <Pressable style={Styles.button} onPress={() => setModalVisible(true)}>
+            <Pressable style={Styles.button} onPress={() => navigation.push("Scan Barcode", {manual: true})}>
                 <Text style={[Styles.button, Styles.buttonText, { backgroundColor: Styles.colors.GEPurple}]}>Manually Enter Patient Info</Text>
             </Pressable>
 
