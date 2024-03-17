06f3de390c281ee5fdcd4c25462ef78c1b9e264d
David Tymoszenko <davidwtymoszenko@gmail.com>
2024-03-17 17:35:10 -0500

    Implemented functioning search bar which filters devices

--- src/components/screens/DeviceSelectScreen.jsx
+++ src/components/screens/DeviceSelectScreen.jsx
@@ -1,12 +1,46 @@
-import { Text, View, Button, Pressable, ScrollView } from "react-native";
+import { Text, View, Button, Pressable, ScrollView, TextInput } from "react-native";
 import Styles from "../../styles/main";
+import { useState } from "react";
 
 function DeviceSelectScreen({ navigation }) {
+    //use state variable to keep track of what is being searched
+    const [currSearch, setCurrSearch] = useState('');
+
+    //Completely changing how this was done, before it was a lot of hardcoded buttons, now going to map
+    //and then filter them based on search use state. Could use ? visibility for each button but that seems
+    //a lot worse than this imo -dt
+    //All hardcoded as of right now.
+    const deviceList = [
+        {name: 'GECP2427170', room: 'Room 412A', isOverride: false },
+        {name: 'GECP4167318', room: 'Room 413B', isOverride: false },
+        {name: 'GECP9834313(override case)', room: 'Room 311C', isOverride: true},
+        {name: 'GECP4934123(override case)', room: 'Room 214A', isOverride: true},
+        {name: 'GECP3018493(override case)', room: 'Room 104D', isOverride: true},
+        {name: 'GECP5813938(override case)', room: 'Room 503C', isOverride: true},
+        {name: 'GECP6847242(override case)', room: 'Room 204E', isOverride: true},
+        {name: 'GECP7892324(override case)', room: 'Room 513B', isOverride: true},
+        {name: 'GECP9342422(override case)', room: 'Room 321A', isOverride: true},
+        {name: 'GECP8432742(override case)', room: 'Room 102F', isOverride: true},
+        {name: 'GECP1032338(override case)', room: 'Room 401C', isOverride: true},
+        {name: 'GECP1238549(override case)', room: 'Room 201A', isOverride: true}
+        
+    ]
+
+    //searched devices is a list of devices which have been filtered based on what is typed
+    //casted everything to lowercase so none of this is case sensitive -dt note 3/17/24 change
+    const searchedDevices = deviceList.filter(device => device.name.toLowerCase().includes(currSearch.toLowerCase()))
 
     return (
         <View style={[Styles.container]}>
             <Text style={[Styles.h4]}><Text style={{color: "white", fontWeight: "bold"}}>Device Select</Text></Text>
             <Text style={[Styles.h6]}>Select a device to continue</Text>
+
+            {/* Added searchbar to search through connectplus devices -dt 3/17/2024 change */}
+            <TextInput
+                placeholder="Type to search..."
+                onChangeText={setCurrSearch}
+                value={currSearch}
+            />
 
             <ScrollView 
                 contentContainerStyle={{
@@ -15,50 +49,17 @@ function DeviceSelectScreen({ navigation }) {
                     paddingBottom: 50
                 }}
             >
-            {/*Using pressable instead of button so we can design, react native is bad w/ natural buttons -DT */}
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: false })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2427170</Text>{"\n"}Room 412A</Text>
-            </Pressable>
-
-            {/*Added button from figma prototype (default case) 3/2/24 -DT*/}
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen")}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP3128689</Text>{"\n"}Room 413B</Text>
-            </Pressable>
-
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-
-            {/*THIS IS HUGEEEE section of placeholder code. This is to show list of devices can be scrollable -DT */}
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-            <Pressable style={Styles.deviceSelectButton} onPress={() => navigation.push("Device Screen", { isOverride: true })}>
-            <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}><Text style={{fontWeight: "bold", fontSize: 16}}>GECP2309701(override case)</Text>{"\n"}Room 311C</Text>
-            </Pressable>
-
-            <Text style={{textAlign: "center", color: "white"}}>Scroll for more devices...</Text>
+                {searchedDevices.map((device) => (
+                    <Pressable 
+                        key={device.name}
+                        style={Styles.deviceSelectButton}
+                        onPress={() => navigation.push("Device Screen", { isOverride: device.isOverride || false })}
+                    >
+                        <Text style={[Styles.deviceSelectButton, Styles.deviceSelectButtonText, { backgroundColor: Styles.colors.GEPurple }]}>
+                            <Text style={{ fontWeight: "bold", fontSize: 16 }}>{device.name}</Text>{"\n"}{device.room}
+                        </Text>
+                    </Pressable>
+                ))}
             </ScrollView>
 
         </View>
