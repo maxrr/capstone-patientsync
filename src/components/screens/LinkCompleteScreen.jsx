43cb97e17195ae1924053654d9578aa660788206
cxhuy <alvinkang1@gmail.com>
2024-04-01 12:45:34 -0500

    feat: add third step stepper

--- src/components/screens/LinkCompleteScreen.jsx
+++ src/components/screens/LinkCompleteScreen.jsx
@@ -5,6 +5,13 @@ function LinkCompleteScreen({ navigation }) {
     return (
         // TODO: On this page, we shouldn't give the user any ability to move backwards ~mr
         <View style={[Styles.container]}>
+            <View style={{ marginBottom: 6, display: "flex", flexDirection: "row", gap: 8, alignItems: "center" }}>
+                <View style={{ width: 40, height: 40, backgroundColor: "green", borderRadius: 8 }} />
+                <View style={{ width: 48, height: 4, backgroundColor: "green", borderRadius: 4 }} />
+                <View style={{ width: 40, height: 40, backgroundColor: "green", borderRadius: 8 }} />
+                <View style={{ width: 48, height: 4, backgroundColor: "green", borderRadius: 4 }} />
+                <View style={{ width: 40, height: 40, backgroundColor: "green", borderRadius: 8 }} />
+            </View>
             <Text style={[Styles.h4]}><Text style={{color: "white", fontWeight: "bold"}}>Success</Text></Text>
             <Text style={[Styles.h6]}>Link complete!</Text>
             <Button title="Return home" onPress={() => navigation.popToTop()} />
--- src/components/screens/LinkConfirmScreen.jsx
+++ src/components/screens/LinkConfirmScreen.jsx
@@ -4,6 +4,13 @@ import Styles from "../../styles/main";
 function LinkConfirmScreen({ navigation }) {
     return (
         <View style={[Styles.container]}>
+            <View style={{ marginBottom: 6, display: "flex", flexDirection: "row", gap: 8, alignItems: "center" }}>
+                <View style={{ width: 40, height: 40, backgroundColor: "green", borderRadius: 8 }} />
+                <View style={{ width: 48, height: 4, backgroundColor: "green", borderRadius: 4 }} />
+                <View style={{ width: 40, height: 40, backgroundColor: "green", borderRadius: 8 }} />
+                <View style={{ width: 48, height: 4, backgroundColor: "green", borderRadius: 4 }} />
+                <View style={{ width: 40, height: 40, backgroundColor: "white", borderRadius: 8 }} />
+            </View>
             <Text style={[Styles.h4]}><Text style={{color: "white", fontWeight: "bold"}}>Link</Text></Text>
             <Text style={[Styles.h6]}>Ready to link?</Text>
             <Button title="Link" onPress={() => navigation.push("Link Complete")} />
