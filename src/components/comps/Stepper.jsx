da611271a6fdc4b256ea3df9334481bec28873f3
cxhuy <alvinkang1@gmail.com>
2024-04-15 16:08:48 -0500

    feat: add stepper step indicator icons

--- src/components/comps/Stepper.jsx
+++ src/components/comps/Stepper.jsx
@@ -3,51 +3,111 @@ import { FontAwesome } from "@expo/vector-icons";
 
 export default function Stepper(props) {
     return (
-        <View style={{ marginBottom: 6, display: "flex", flexDirection: "row", gap: 8, alignItems: "center" }}>
-            <View
-                style={{
-                    width: 40,
-                    height: 40,
-                    backgroundColor: props.step > 1 ? "green" : props.step === 1 ? "white" : "gray",
-                    borderRadius: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}
-            >
-                <FontAwesome name="bluetooth" size={24} color={props.step > 1 ? "white" : "black"} />
+        <>
+            <View style={{ display: "flex", flexDirection: "row", gap: 8, alignItems: "center" }}>
+                <View
+                    style={{
+                        width: 40,
+                        height: 15,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}
+                >
+                    {props.step > 1 ? (
+                        <FontAwesome name="check-circle" size={15} color="green" />
+                    ) : props.step == 1 ? (
+                        <FontAwesome name="caret-down" size={15} color="white" />
+                    ) : (
+                        <></>
+                    )}
+                </View>
+                <View style={{ width: 48, height: 4 }} />
+                <View
+                    style={{
+                        width: 40,
+                        height: 15,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}
+                >
+                    {props.step > 2 ? (
+                        <FontAwesome name="check-circle" size={15} color="green" />
+                    ) : props.step == 2 ? (
+                        <FontAwesome name="caret-down" size={15} color="white" />
+                    ) : (
+                        <></>
+                    )}
+                </View>
+                <View style={{ width: 48, height: 4 }} />
+                <View
+                    style={{
+                        width: 40,
+                        height: 15,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}
+                >
+                    {props.step > 3 ? (
+                        <FontAwesome name="check-circle" size={15} color="green" />
+                    ) : props.step == 3 ? (
+                        <FontAwesome name="caret-down" size={15} color="white" />
+                    ) : (
+                        <></>
+                    )}
+                </View>
             </View>
-            <View style={{ width: 48, height: 4, backgroundColor: "gray", borderRadius: 4 }} />
-            <View
-                style={{
-                    width: 40,
-                    height: 40,
-                    backgroundColor: props.step > 2 ? "green" : props.step === 2 ? "white" : "gray",
-                    borderRadius: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}
-            >
-                <FontAwesome name="user" size={24} color={props.step > 2 ? "white" : "black"} />
+            <View style={{ marginBottom: 6, display: "flex", flexDirection: "row", gap: 8, alignItems: "center" }}>
+                <View
+                    style={{
+                        width: 40,
+                        height: 40,
+                        backgroundColor: props.step > 1 ? "green" : props.step === 1 ? "white" : "gray",
+                        borderRadius: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}
+                >
+                    <FontAwesome name="bluetooth" size={24} color={props.step > 1 ? "white" : "black"} />
+                </View>
+                <View style={{ width: 48, height: 4, backgroundColor: "gray", borderRadius: 4 }} />
+                <View
+                    style={{
+                        width: 40,
+                        height: 40,
+                        backgroundColor: props.step > 2 ? "green" : props.step === 2 ? "white" : "gray",
+                        borderRadius: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}
+                >
+                    <FontAwesome name="user" size={24} color={props.step > 2 ? "white" : "black"} />
+                </View>
+                <View style={{ width: 48, height: 4, backgroundColor: "gray", borderRadius: 4 }} />
+                <View
+                    style={{
+                        width: 40,
+                        height: 40,
+                        backgroundColor: props.step > 3 ? "green" : props.step === 3 ? "white" : "gray",
+                        borderRadius: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}
+                >
+                    <FontAwesome name="link" size={24} color={props.step > 3 ? "white" : "black"} />
+                </View>
             </View>
-            <View style={{ width: 48, height: 4, backgroundColor: "gray", borderRadius: 4 }} />
-            <View
-                style={{
-                    width: 40,
-                    height: 40,
-                    backgroundColor: props.step > 3 ? "green" : props.step === 3 ? "white" : "gray",
-                    borderRadius: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}
-            >
-                <FontAwesome name="link" size={24} color={props.step > 3 ? "white" : "black"} />
-            </View>
-        </View>
+        </>
     );
 }
